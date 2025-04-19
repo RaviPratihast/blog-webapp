1c71ad802ecb2ef9ad8d3a12e54d568ac3c9cfde
Ravi Pratihast <ravipratihast@gmail.com>
2025-04-20 00:33:59 +0530

    fix: remove unwanted code.

--- app/constants/resources.ts
+++ app/constants/resources.ts
@@ -30,22 +30,3 @@ export function createResource(
     },
   };
 }
-
-export const resources: Resource[] = [
-  createResource(
-    1,
-    "Article",
-    "Sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "Quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto",
-    "John Doe",
-    "Mar 16, 2024"
-  ),
-  createResource(
-    2,
-    "Article",
-    "Qui est esse",
-    "Est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla",
-    "Jane Smith",
-    "Mar 16, 2024"
-  ),
-];
